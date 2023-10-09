d503f08b56a99ea1ac60669ef67c310e1832ae8e
Samir <125750491+sam-k9@users.noreply.github.com>
2023-10-09 18:38:09 +0530

    FormContainer

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -17,41 +17,40 @@ const Form = () => {
   };
 
   return (
-    <form
-      onSubmit={onSUbmit}
-      className="py-4 px-4 flex justify-between bg-gray-900"
-    >
-      <label className="text-lg font-medium text-gray-100">
-        Title
-        <input
-          required
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          className="text-gray-900 bg-blue-100 ml-6 py-2 px-2 focus:outline-none rounded-md border-gray-400 border-l-2"
-        />
-      </label>
-      <label className="text-lg font-medium text-gray-100">
-        Tags{" "}
-        <span className="text-sm font-normal">
-          (upto 3 | space b/w each tag)
-        </span>
-        <input
-          required
-          type="text"
-          value={tags}
-          onChange={(e) => {
-            if (e.target.value.split(" ").length < 4) setTags(e.target.value);
-          }}
-          className="text-gray-900 bg-blue-100 ml-6 py-2 px-2 focus:outline-none rounded-md border-gray-400 border-l-2 text"
-        />
-      </label>
-      <button
-        type="submit"
-        className="px-4 rounded-xl font-semibold text-white bg-cyan-500"
-      >
-        Create Todo
-      </button>
+    <form onSubmit={onSUbmit} className=" bg-gray-900">
+      <div className="w-[1000px] m-auto py-4 px-4 flex justify-between bg-gray-900">
+        <label className="text-lg font-medium text-gray-100">
+          Title
+          <input
+            required
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="text-gray-900 bg-blue-100 ml-6 py-2 px-2 focus:outline-none rounded-md border-gray-400 border-l-2"
+          />
+        </label>
+        <label className="text-lg font-medium text-gray-100">
+          Tags{" "}
+          <span className="text-sm font-normal">
+            (upto 3 | space b/w each tag)
+          </span>
+          <input
+            required
+            type="text"
+            value={tags}
+            onChange={(e) => {
+              if (e.target.value.split(" ").length < 4) setTags(e.target.value);
+            }}
+            className="text-gray-900 bg-blue-100 ml-6 py-2 px-2 focus:outline-none rounded-md border-gray-400 border-l-2 text"
+          />
+        </label>
+        <button
+          type="submit"
+          className="px-4 rounded-xl font-semibold text-white bg-cyan-500"
+        >
+          Create Todo
+        </button>
+      </div>
     </form>
   );
 };
